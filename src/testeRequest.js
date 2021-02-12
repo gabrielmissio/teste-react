d89dc477fc7472bc5949d400b2672e3d0541e6a6
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-02-12 15:50:44 -0300

    first commit

---
+++ src/testeRequest.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import axios from 'axios';
+
+function Teste() {
+    
+    const axios = require('axios').default;
+    var teste1 = 'https://oumbd5l1x3.execute-api.us-east-1.amazonaws.com/dev/register'
+    var teste2 = 'https://jsonplaceholder.typicode.com/posts'
+    var teste3 = 'https://127.0.0.1:3000/register'
+    var texto = 'um texto qualquer'
+    axios.get(teste1)
+    .then(resp => {
+        console.log(resp.data);
+        texto = resp.data;
+        
+    })
+    .catch(err => {
+        // Handle Error Here
+        console.error(err);
+    });
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              { texto }
+            </p>
+          
+          </header>
+        </div>
+      );
+    
+}
+  
+  export default Teste;
