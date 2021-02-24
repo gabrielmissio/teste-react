b9befc1c9416293a7bf1276a97ef24a52618b1c5
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-02-24 14:34:34 -0300

    Enviando post request com sucesso

--- src/App.js
+++ src/App.js
@@ -13,21 +13,33 @@ function App() {
   const [submitting, setSubmitting] = useState(false);
 
   const handleSubmit = event => {
-    alert('description: ' + event.target.description.value + '\nvalue: ' + event.target.value.value + '\ntype: '+ event.target.type.value + '\ntag_id: ' + event.target.tag_id.value)
+    var description = event.target.description.value
+    var value = event.target.value.value
+    var type = event.target.type.value
+    var tag_id = event.target.tag_id.value
     event.preventDefault();
     setSubmitting(true);
-    const axios = require('axios').default;
-    var teste1 = 'https://oumbd5l1x3.execute-api.us-east-1.amazonaws.com/dev/register'
-    axios.get(teste1)
-    .then(resp => {
-        console.log(resp.data);
-        //setSubmitting(false);
-        
+    var axios = require('axios');
+    var data = JSON.stringify({"description":description,"value":value,"type":type,"tags":[{"tag":tag_id}]});
+    
+    var config = {
+      method: 'post',
+      url: 'https://oumbd5l1x3.execute-api.us-east-1.amazonaws.com/hml/register',
+      headers: { 
+        'Authorization': 'Bearer eyJraWQiOiJzbmEyYmFlNGtMbnhpODdPOU5nRDlkd3NzOWRJN2lPNExzQUd3c0lCa1cwPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1NWMyMzQ2NC1mZWIxLTRkOWItYmViOC0yYzQ3YmEwMTFkNDQiLCJjb2duaXRvOmdyb3VwcyI6WyJsdWNraWUtdGVjaCJdLCJldmVudF9pZCI6IjI1MGM5OThhLTIwYTMtNDc0Zi1hNGNmLThjNjQ4N2ZiYThmYiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTM3NjcxMTIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX1hwUGtubkpzciIsImV4cCI6MTYxMzc3MDcxMiwiaWF0IjoxNjEzNzY3MTEyLCJqdGkiOiJiMGVjOWE4OC1hZmEzLTQ1MTQtOWEyYS04NjUxYTM5NmZhY2MiLCJjbGllbnRfaWQiOiI3MTNqcGgwZWxnc291ZTltY211M2xnY2o1aSIsInVzZXJuYW1lIjoidGhpYWdvLm0ifQ.LNtqXXWxyIDkQEKktocWTNj-raVOVsbK71m8J4_Ijy06_eD9CzYxqa4S7rhRaew-6Ql5NPH4gIDOfUHtkd42ttCZOuiphk8AtetpLuWuwB_M-3QWo68YWevW-X1hgn_fx9AXt9B8kz2FDbN6g2gylQ8jrEPdaB-tnnR5PkcXTZB7pKLz8moX_qB2Sfe8OTMey6abV1nEzrQ9CqPWPMklrR_CsEa2G4OZZdRO9orPRa9E5tmxXdaSH7hzR6cH6lSe4yb3ACJ-AxLB-ZsUpbkV0oLNmP3sDk3gB_uWO2TYpuZ-7FFqUYqZRzeYW1-SYvL7WCPQJHxQMDXzCA_sG4eqeA', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
     })
-    .catch(err => {
-        // Handle Error Here
-        console.error(err);
+    .catch(function (error) {
+      console.log(error);
     });
+    
   }
 
   const handleChange = event => {
