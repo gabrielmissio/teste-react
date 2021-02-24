f5fcacc7ff432bde9f0b6896a43741438019eba0
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-02-24 13:19:44 -0300

    adecionado fields necessarios para testar post request

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+  return {
+   ...state,
+   [event.name]: event.value
+ }
+}
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
+
   const handleSubmit = event => {
+    alert('description: ' + event.target.description.value + '\nvalue: ' + event.target.value.value + '\ntype: '+ event.target.type.value + '\ntag_id: ' + event.target.tag_id.value)
     event.preventDefault();
     setSubmitting(true);
-
     const axios = require('axios').default;
     var teste1 = 'https://oumbd5l1x3.execute-api.us-east-1.amazonaws.com/dev/register'
     axios.get(teste1)
     .then(resp => {
         console.log(resp.data);
-        setSubmitting(false);
+        //setSubmitting(false);
         
     })
     .catch(err => {
         // Handle Error Here
         console.error(err);
     });
- }
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
 
   return(
     <div className="wrapper">
-      <h1>How About Them Apples</h1>
+      <h1>Spending Analyzer</h1>
       {submitting &&
-       <div>Submtting Form...</div>
+       <div>
+         You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+           ))}
+         </ul>
+       </div>
       }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
-            <p>Name</p>
-            <input name="name" />
+            <p>Description</p>
+            <input name="description" onChange={handleChange}/>
+            <p>Value</p>
+            <input name="value" onChange={handleChange}/>
+            <p>Type</p>
+            <input name="type" onChange={handleChange}/>
+            <p>Tag Id</p>
+            <input name="tag_id" onChange={handleChange}/>
           </label>
         </fieldset>
         <button type="submit">Submit</button>
