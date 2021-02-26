e4be801241422b680bc833ae528e506fd629babf
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-02-26 17:58:57 -0300

    -

--- src/TableRegister.js
+++ src/TableRegister.js
@@ -52,7 +52,8 @@ async function getOptions(){
     alert('aqui eu')
     var teste = []
     options.map(v => teste.push(createData(v.description, v.type, 1324171354, 3287263)))
-    return options
+    console.log('processou')
+    return teste
 }
   
 
@@ -63,20 +64,8 @@ function createData(name, code, population, size) {
 
 const rows = [
   createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData('TESTE', 'AA', 1403500365, 9596961),
+
 ];
 
 const useStyles = makeStyles({
