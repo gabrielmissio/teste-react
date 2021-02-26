6cbf4844ca90dc95646da02f8b1583cdb6604689
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-02-26 17:04:33 -0300

    Guardando valores do banco em formato para exebicao

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import App from './FormCreateTag';
+import StickyHeadTable from './TableRegister';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StickyHeadTable />
   </React.StrictMode>,
   document.getElementById('root')
 );
